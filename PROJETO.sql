a3d43f4d0bb02099f2d596720011e7b49b489f20
Breno Eduardo <58312601+breno-eduardo@users.noreply.github.com>
2023-01-25 21:05:04 -0300

    Add files via upload

---
+++ PROJETO.sql
@@ -0,0 +1,20 @@
+SHOW DATABASES;
+USE bd_nba;
+
+CREATE TABLE detalhados
+(nome varchar(50),
+pontos INT(30),
+mvp INT(5),
+team VARCHAR(50),
+abreviacao varchar(10),
+arena varchar(35),
+cidade varchar(40),
+draft varchar(4)
+);
+
+SELECT * FROM detalhados;
+
+
+
+
+ 
